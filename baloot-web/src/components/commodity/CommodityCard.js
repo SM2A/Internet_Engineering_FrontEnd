926dfd6b79fa940ee5eb155dde66ee75853467c5
morteza-nouri <morteza1noori@gmail.com>
2023-05-03 01:21:54 +0430

    improve style

--- baloot-web/src/components/commodity/CommodityCard.js
+++ baloot-web/src/components/commodity/CommodityCard.js
@@ -2,15 +2,15 @@ import "../../assets/styles/CommodityCard.css";
 
 export default function CommodityCard({commodity}) {
     return (
-        <div className="commodity-card">
-            <div className="commodity-card-header">
-                <h2>{commodity.name}</h2>
-                <p>{commodity.inStock} left in stock</p>
+        <div className="card col-md-3">
+            <div className="card-body">
+                <h2 className="card-title">{commodity.name}</h2>
+                <p className="card-text">{commodity.inStock} left in stock</p>
             </div>
             {/*todo: replace this with commodity.imageUrl*/}
-            <img src={require("../../assets/images/phone.png")} alt={commodity.name}/>
+            <img className="img-fluid" src={require("../../assets/images/phone.png")} alt={commodity.name}/>
             <div className="commodity-card-footer">
-                <p>{commodity.price}$</p>
+                <p>{commodity.price}</p>
                 <button>Add to Cart</button>
             </div>
         </div>
