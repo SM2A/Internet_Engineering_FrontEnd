22b05173aefc9d3991f65776d40f2204509acd7d
morteza-nouri <morteza1noori@gmail.com>
2023-05-04 17:04:38 +0430

    add provider page

--- baloot-web/src/App.js
+++ baloot-web/src/App.js
@@ -9,6 +9,8 @@ import Footer from "./components/Footer";
 import SignupPage from "./pages/Signup";
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
+import Commodity from "./pages/Commodity";
+import Provider from "./pages/Provider";
 
 const notify = (message) => toast(message);
 
@@ -19,6 +21,8 @@ function App() {
             <Routes>
                 <Route element={<MainLayout/>}>
                     <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/commodities/:id" element={<Commodity/>}/>
+                    <Route exact path="/providers/:id" element={<Provider/>}/>
                 </Route>
                 <Route exact path="/login" element={<LoginPage notify={notify}/>}/>
                 <Route exact path="/signup" element={<SignupPage notify={notify}/>}/>
