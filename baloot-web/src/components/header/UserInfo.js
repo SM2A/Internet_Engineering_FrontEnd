7089a4852bfe50e737c64406267b45b125a22de6
morteza-nouri <morteza1noori@gmail.com>
2023-05-04 22:53:43 +0430

    do refactor

--- baloot-web/src/components/header/UserInfo.js
+++ baloot-web/src/components/header/UserInfo.js
@@ -2,10 +2,10 @@ import "../../assets/styles/header/UserInfo.css";
 
 export default function UserInfo({user}) {
     return (
-        <div className="header__userinfo">
-            {/*todo: replace with user info*/}
-            <h3>#username</h3>
-            <button type="button" className="btn btn-info">Cart 0</button>
-        </div>
+            <div className="d-flex justify-content-end header__userinfo">
+                {/*todo: replace with user info*/}
+                <h3>{user.username}</h3>
+                <button type="button" className="btn btn-info">Cart 0</button>
+            </div>
     );
 }
