fc3593603669ca463ad75d736765f9041bb8bf64
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2023-06-02 17:16:10 +0330

    Add authorization header

--- baloot-web/src/layouts/MainLayout.js
+++ baloot-web/src/layouts/MainLayout.js
@@ -5,9 +5,12 @@ import {useEffect, useState} from "react";
 export default function MainLayout() {
     const [user, setUser] = useState({});
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    let token = localStorage.getItem("token")
 
     function getLoggedInUser() {
-        fetch("http://localhost:8080/api/users/loggedInUser")
+        fetch("http://localhost:8080/api/users/loggedInUser", {
+            headers: {'Authorization': `Bearer ${token}`}
+        })
             .then((resp) => {
                 if (resp.ok) {
                     setIsLoggedIn(true);
